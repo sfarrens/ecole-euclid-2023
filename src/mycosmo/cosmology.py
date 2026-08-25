6382cb681a190ea44f7b93c5e9b3e214aac14278
sfarrens <samuel.farrens@gmail.com>
2026-08-25 12:13:39 +0200

    Add API documentation and docstrings

--- src/mycosmo/cosmology.py
+++ src/mycosmo/cosmology.py
@@ -1,3 +1,9 @@
+"""Cosmology.
+
+This module implements various cosmology routines.
+
+"""
+
 import numpy as np
 
 from .constants import G, Mpc
@@ -6,6 +12,50 @@ from .constants import G, Mpc
 def hubble(
     redshift: float | np.ndarray, cosmo_dict: dict[str, float]
 ) -> float | np.ndarray:
+    r"""Hubble Parameter.
+
+    Calculate the Hubble parameter at a given redshift using the cosmological
+    parameter values provided.
+
+    Parameters
+    ----------
+    redshift : float or numpy.ndarray
+        Redshift(s) at which the Hubble parameter should be calculated
+    cosmo_dict : dict
+        Dictionary of cosmological constants. Must contain the following keys:
+
+        * ``H0``: The Hubble parameter value at redshift zero.
+        * ``omega_m_0``: The matter density at redshift zero.
+        * ``omega_k_0``: The curvature density at redshift zero.
+        * ``omega_lambda_0``: The dark energy density at redshift zero.
+
+    Returns
+    -------
+    float or numpy.ndarray
+        Value of the Hubble parameter (km/s/Mpc) at the specified redshift(s)
+        for a given cosmology.
+
+    Notes
+    -----
+    This function implements the calculation of the Hubble parameter as follows:
+
+    .. math::
+        H(z) = \sqrt{H_0^2 (\Omega_{m,0}(1+z)^3 + \Omega_{k,0}(1+z)^2 +
+            \Omega_{\Lambda,0})}
+
+    Examples
+    --------
+    >>> from mycosmo.cosmology import hubble
+    >>> cosmo_dict = {
+    ...     "H0": 70,
+    ...     "omega_m_0": 0.3,
+    ...     "omega_k_0": 0.0,
+    ...     "omega_lambda_0": 0.7,
+    ... }
+    >>> float(hubble(0.0, cosmo_dict))
+    70.0
+
+    """
     hubble_const = cosmo_dict["H0"]
     matter = cosmo_dict["omega_m_0"] * (1 + redshift) ** 3
     curvature = cosmo_dict["omega_k_0"] * (1 + redshift) ** 2
