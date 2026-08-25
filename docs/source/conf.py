5630d595af30e93543d76944a9b81402cabfdf08
sfarrens <samuel.farrens@gmail.com>
2026-08-25 17:06:16 +0200

    Fix formatting caught by pre-commit run --all-files

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,12 +6,12 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'mycosmo'
-copyright = '2026, sfarrens'
-author = 'sfarrens'
+project = "mycosmo"
+copyright = "2026, sfarrens"
+author = "sfarrens"
 
-version = '0.1.0'
-release = '0.1.0'
+version = "0.1.0"
+release = "0.1.0"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
@@ -25,16 +25,16 @@ extensions = [
     "numpydoc",
 ]
 
-templates_path = ['_templates']
+templates_path = ["_templates"]
 exclude_patterns = []
 
-language = 'en'
+language = "en"
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = "sphinx_book_theme"
-html_static_path = ['_static']
+html_static_path = ["_static"]
 
 intersphinx_mapping = {
     "python": ("http://docs.python.org/3", None),
